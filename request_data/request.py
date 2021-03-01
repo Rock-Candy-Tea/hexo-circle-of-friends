578a7189cb3134d836e54a5b2ade95a7fe9cb1ac
Zfour <499984532@qq.com>
2021-03-01 14:48:00 +0800

    增加了网络请求配置

--- request_data/request.py
+++ request_data/request.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
 import requests
+import yaml
 requests.packages.urllib3.disable_warnings()
 
+def load_config():
+    f = open('_config.yml', 'r',encoding='gbk')
+    ystr = f.read()
+    ymllist = yaml.load(ystr, Loader=yaml.FullLoader)
+    return ymllist
+
 def get_data(link):
+    config = load_config()
     result = ''
     user_agent = 'Mozilla/5.0 (Macintosh;Intel Mac OS X 10_12_6) ' \
                  'AppleWebKit/537.36(KHTML, like Gecko) ' \
                  'Chrome/67.0.3396.99Safari/537.36'
     header = {'User_Agent': user_agent}
     try:
-        r = requests.get(link, headers=header, timeout=15,verify=False)
+        r = requests.get(link, headers=header, timeout=config['setting']['request']['timeout'],verify=config['setting']['request']['ssl'])
         r.encoding = 'utf-8'
         result = r.text.encode("gbk", 'ignore').decode('gbk', 'ignore')
         if str(r) == '<Response [404]>':
@@ -19,5 +27,3 @@ def get_data(link):
         print(e.__traceback__.tb_frame.f_globals["__file__"])
         print(e.__traceback__.tb_lineno)
     return result
-
-get_data('https://nekodeng.gitee.io/')
