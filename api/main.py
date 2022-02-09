34fef70055f1404a88c971fd47686de82797f24e
yyyz <904108079@qq.com>
2022-02-09 22:33:40 +0800

    release 4.2.1

--- api/main.py
+++ api/main.py
@@ -1,5 +1,4 @@
 # -*- coding:utf-8 -*-
-import requests
 from lxml import etree
 import uvicorn
 import sys
@@ -30,49 +29,49 @@ app.add_middleware(
 
 
 @app.get("/all", tags=["API"], summary="返回完整统计信息")
-async def all(start: int = 0, end: int = -1, rule: str = "updated"):
+def all(start: int = 0, end: int = -1, rule: str = "updated"):
     '''返回数据库统计信息和文章信息列表
     - start: 文章信息列表从 按rule排序后的顺序 的开始位置
     - end: 文章信息列表从 按rule排序后的顺序 的结束位置
     - rule: 文章排序规则（创建时间/更新时间）
     '''
     list = ['title', 'created', 'updated', 'link', 'author', 'avatar']
-    return await query_all(list, start, end, rule)
+    return query_all(list, start, end, rule)
 
 
 @app.get("/friend", tags=["API"], summary="返回所有友链")
-async def friend():
+def friend():
     '''返回数据库友链列表
     '''
-    return await query_friend()
+    return query_friend()
 
 
 @app.get("/randomfriend", tags=["API"], summary="返回随机友链")
-async def random_friend():
+def random_friend():
     '''随机返回一个友链信息
     '''
-    return await query_random_friend()
+    return query_random_friend()
 
 
 @app.get("/randompost", tags=["API"], summary="返回随机文章")
-async def random_post():
+def random_post():
     '''随机返回一篇文章信息
     '''
-    return await query_random_post()
+    return query_random_post()
 
 
 @app.get("/post", tags=["API"], summary="返回指定链接的所有文章")
-async def post(link: str = None, num: int = -1, rule: str = "created"):
+def post(link: str = None, num: int = -1, rule: str = "created"):
     '''返回指定链接的数据库内文章信息列表
     - link: 链接地址
     - num: 指定链接的文章信息列表 按rule排序后的顺序的前num篇
     - rule: 文章排序规则（创建时间/更新时间）
     '''
-    return await query_post(link, num, rule)
+    return query_post(link, num, rule)
 
 
 @app.get("/postjson", tags=["API"], summary="返回指定所有链接的所有文章")
-async def postjson(jsonlink: str, start: int = 0, end: int = -1, rule: str = "updated"):
+def postjson(jsonlink: str, start: int = 0, end: int = -1, rule: str = "updated"):
     '''获取公共库中指定链接列表的文章信息列表
     - jsonlink: 友链链接json的cdn地址
     - start: 文章信息列表从 按rule排序后的顺序 的开始位置
@@ -80,7 +79,7 @@ async def postjson(jsonlink: str, start: int = 0, end: int = -1, rule: str = "up
     - rule: 文章排序规则（创建时间/更新时间）
     '''
     list = ['title', 'created', 'updated', 'link', 'author', 'avatar']
-    return await query_post_json(jsonlink, list, start, end, rule)
+    return query_post_json(jsonlink, list, start, end, rule)
 
 @app.get("/version", tags=["version"], summary="返回版本信息")
 def version():
