7da4790677a68514982986566295fbdc9079b5f8
Zkeq <admin@icodeq.com>
2022-05-09 21:22:18 +0800

    修复获取到的友链个数与显示个数不同导致的前端报错
    详情见此 API : 
    https://hexo-circle-of-friends-6byyqjphz-zkeq.vercel.app/post?num=2&link=https://coding.maylove.pub

--- api/leancloudapi.py
+++ api/leancloudapi.py
@@ -213,7 +213,7 @@ def query_post(link, num, rule):
         "author": author,
         "link": link,
         "avatar": avatar,
-        "article_num": article_num
+        "article_num": num # 详情见229行，取的是前num个元素，这里要显示一致，否则会前端触发报错
     }
 
     if num < 0 or num > min(article_num, 1000):
