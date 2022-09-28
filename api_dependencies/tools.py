6a3e1a387b83bd0294ba1056ca811cc05f2dc0e3
yyyz <904108079@qq.com>
2022-09-28 10:05:44 +0800

    fix vercel+github dump settings

--- api_dependencies/tools.py
+++ api_dependencies/tools.py
@@ -4,5 +4,10 @@ import os
 from hexo_circle_of_friends.utils.project import get_user_settings
 
 
+def is_vercel_sqlite():
+    settings = get_user_settings()
+    return os.environ.get("VERCEL") and settings["DATABASE"] == "sqlite"
+
+
 def is_vercel():
     return os.environ.get("VERCEL")
