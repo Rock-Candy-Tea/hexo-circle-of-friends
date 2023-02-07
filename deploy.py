f0c5058676d3e1a3f34740115ff4ab2dd41ea6ed
yyyz <904108079@qq.com>
2023-02-07 17:41:55 +0800

    change docker build options

--- deploy.py
+++ deploy.py
@@ -35,8 +35,8 @@ def server_deploy(port):
             port = 8000
         server_sh += f"export EXPOSE_PORT={port}"
         server_sh += """
-        nohup python3 -u ${BASE_PATH}/hexo_circle_of_friends/run.py > /dev/null &
-        nohup python3 -u ${BASE_PATH}/api/main.py > /dev/null &
+        nohup python3 -u ${BASE_PATH}/hexo_circle_of_friends/run.py > /dev/null 2>&1 &
+        nohup python3 -u ${BASE_PATH}/api/main.py > /dev/null 2>&1 &
             """
         f.write(server_sh.strip())
     os.system("chmod a+x temp.sh && ./temp.sh && rm -f temp.sh")
@@ -72,9 +72,9 @@ while 1:
                 docker_port = 8000
             os.system(f"docker run -di --name circle -p {docker_port}:8000 -v /tmp/:/tmp/ yyyzyyyz/fcircle:latest")
             # os.system("docker exec circle nohup python3 -u ./hexo_circle_of_friends/run.py > /tmp/crawler_stdout.log 2>&1 &")
-            os.system("docker exec circle nohup python3 -u ./hexo_circle_of_friends/run.py > /dev/null &")
+            os.system("docker exec circle nohup python3 -u ./hexo_circle_of_friends/run.py > /dev/null 2>&1 &")
             # os.system("docker exec circle nohup python3 -u ./api/main.py > /tmp/api_stdout.log 2>&1 &")
-            os.system("docker exec circle nohup python3 -u ./api/main.py > /dev/null &")
+            os.system("docker exec circle nohup python3 -u ./api/main.py > /dev/null 2>&1 &")
             print("已部署！")
         elif r == "2":
             os.system("docker stop circle && docker rm circle && docker rmi yyyzyyyz/fcircle:latest")
